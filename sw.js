84a6b66887f653c67d16a3a3b0a9b111dc60a41c
yeezyluck <yeezyluck@gmail.com>
2025-12-31 03:50:03 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+
+const CACHE = "osrs-pwa-v1";
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll(["./", "./index.html", "./script.js"])
+    )
+  );
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
